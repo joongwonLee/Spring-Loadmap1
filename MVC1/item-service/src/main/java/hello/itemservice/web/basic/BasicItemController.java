0ef648df7a61aeaa6e17ab2731d2a29a68b839de
joongwon <wnddnjs823@ajou.ac.kr>
2023-11-07 01:42:46 +0900

    [스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술]  sec7. 상품 상세, 상품 등록 폼

--- MVC1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ MVC1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -34,6 +36,28 @@ public class BasicItemController {
         return "basic/items";
     }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+
+        return "/basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+
+        return "/basic/addForm";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
